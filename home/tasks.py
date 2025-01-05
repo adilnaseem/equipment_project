2becfcedfe597c9192b95bd8fbdfde700189c992
adilnaseem <adilnaseempk@yahoo.com>
2025-01-05 23:08:23 +0500

    update

--- home/tasks.py
+++ home/tasks.py
@@ -1,15 +1,33 @@
-# myapp/tasks.py
+# Create your tasks here
+
+# from demoapp.models import Widget
 
 from celery import shared_task
+
 
 @shared_task
 def add(x, y):
     return x + y
 
+
 @shared_task
 def mul(x, y):
     return x * y
 
+
 @shared_task
 def xsum(numbers):
     return sum(numbers)
+
+# class Widget(models.Model):
+#     name = models.CharField(max_length=140)
+# @shared_task
+# def count_widgets():
+#     return Widget.objects.count()
+
+
+# @shared_task
+# def rename_widget(widget_id, name):
+#     w = Widget.objects.get(id=widget_id)
+#     w.name = name
+#     w.save()
